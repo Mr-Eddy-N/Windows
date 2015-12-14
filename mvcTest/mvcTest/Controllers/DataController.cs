7f4931a9e7570a9c0295d7486167f6a8e533983f
edgar7386 <edgarreyesrojas@gmail.com>
2015-12-13 21:27:33 -0600

    conexion a DB

---
+++ mvcTest/mvcTest/Controllers/DataController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcTest.Controllers
+{
+    public class DataController : Controller
+    {
+        //
+        // GET: /Data/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Holi() {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult setData(string str){
+           
+        string message="msss";
+            JsonResult jsonr= new JsonResult{Data=message, JsonRequestBehavior= JsonRequestBehavior.AllowGet};
+            return jsonr;
+        }
+
+    }
+}
---
+++ mvcTest/mvcTest/Controllers/HolaController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcTest.Controllers
+{
+    public class HolaController : Controller
+    {
+        //
+        // GET: /Hola/
+
+        public ActionResult Index()
+        {
+           return View();
+           // return Content("Holi mdfk!!!!");
+        }
+        [HttpPost]
+        public JsonResult setData(Form form)
+        {
+            try
+            {
+                string strcxn = ConfigurationManager.ConnectionStrings["BibliotecaConnectionString"].ToString();
+                SqlConnection sqlConnection = new SqlConnection(strcxn);
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "InsertBook";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = sqlConnection;
+                cmd.Parameters.Add("@titulo", SqlDbType.NVarChar).Value = form.Nombre.ToString();
+                cmd.Parameters.Add("@autor", SqlDbType.NVarChar).Value = form.Autor.ToString();
+                cmd.Parameters.Add("@ISBN", SqlDbType.NVarChar).Value = form.ISBN.ToString();
+                cmd.Parameters.Add("@comentarios", SqlDbType.NVarChar).Value = form.Comentarios.ToString();
+                sqlConnection.Open();
+                cmd.ExecuteScalar();
+                sqlConnection.Close();
+                JsonResult jsonr = new JsonResult { Data = "true", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+                return jsonr;
+            }
+            catch (Exception ex) {
+                JsonResult jsonr = new JsonResult { Data = ex.ToString(), JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+                return jsonr;     
+            }
+        }
+
+    }
+    public class Form {
+        public string Nombre { get; set; }
+        public string Autor { get; set; }
+        public string ISBN { get; set; }
+        public string Comentarios { get; set; }
+    
+    }
+}
